79ce1fb2f804e103140e070e4fad25e98bf665c9
M Alexander Moroney <alexmoroney@gmail.com>
2022-07-11 22:55:23 -0500

    Set up React App Scaffolding

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,8 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+import React from "react";
+
+const SeasonDisplay = () => {
+  return <div>Season Display</div>;
+};
+
+export default SeasonDisplay;
